0f38587a2a087d2c3d5a53c134417de6f71653ab
Sean Craig <fwcycling@gmail.com>
2015-08-10 10:47:54 -0500

    Assignment 19 Storyboard Alcolator complete

---
+++ Alcolator/Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Sean Craig on 8/10/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
